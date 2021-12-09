56161229f23a94239db3537702e6637fd983799c
HeulwenDo <52563541+HeulwenDo@users.noreply.github.com>
2021-12-09 08:50:33 +0700

    demo

--- predict_on_sentence.py
+++ predict_on_sentence.py
@@ -1,6 +1,6 @@
 tokenizer = AutoTokenizer.from_pretrained('./un-ner.model/')
 
-paragraph = '''Before proceeding further, I should like to inform members that action on draft resolution iv, entitled situation of human rights of Rohingya Muslims and other minorities in Myanmar is postponed to a later date to allow time for the review of its programme budget implications by the fifth committee. The assembly will take action on draft resolution iv as soon as the report of the fifth committee on the programme budget implications is available. I now give the floor to delegations wishing to deliver explanations of vote or position before voting or adoption.'''
+paragraph = '''Hà Nội ngày hôm nay có 367 ca Covid-19 tại quận Thanh Xuân.'''
 tokens = tokenizer(paragraph)
 torch.tensor(tokens['input_ids']).unsqueeze(0).size()
 
