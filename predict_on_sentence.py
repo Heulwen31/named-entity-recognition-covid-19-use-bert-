93da686300c5850a354419931e65f13ff42e2570
HeulwenDo <52563541+HeulwenDo@users.noreply.github.com>
2021-12-12 15:16:44 +0700

    fix error

--- predict_on_sentence.py
+++ predict_on_sentence.py
@@ -7,7 +7,6 @@ torch.tensor(tokens['input_ids']).unsqueeze(0).size()
 model = AutoModelForTokenClassification.from_pretrained('./un-ner.model/', num_labels=len(label_list))
 predictions = model.forward(input_ids=torch.tensor(tokens['input_ids']).unsqueeze(0), attention_mask=torch.tensor(tokens['attention_mask']).unsqueeze(0))
 predictions = torch.argmax(predictions.logits.squeeze(), axis=1)
-predictions = [label_list[i] for i in preds]
 
 words = tokenizer.batch_decode(tokens['input_ids'])
 pd.DataFrame({'ner': predictions, 'words': words}).to_csv('un_ner.csv')
